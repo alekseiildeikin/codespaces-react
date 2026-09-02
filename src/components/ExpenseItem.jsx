bbe69f841d3463b80bbbc3086b20a1d7c7c53a01
alekseiildeikin <aleksei.ildeikin@voco.ee>
2026-09-02 08:43:01 +0000

    Done

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+    const date = new Date(2024, 10, 12)
+    const title = 'New book'
+    const price = 30.99
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>[{date.toString()}]</div>
             <div className='expense-item_description'>
                 <h2>Title</h2>
-                <div className='expense-item_price'>Price</div>
+                <div className='expense-item_price'>{price}</div>
             </div>
         </div>
     )
