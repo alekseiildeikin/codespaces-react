5e3f2038143edce2637b701cbfc542ed250fd5a6
alekseiildeikin <aleksei.ildeikin@voco.ee>
2026-09-09 08:36:46 +0000

    Added expenses

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,14 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+const Expenses = (props) => {
+
+    return (
+        <div className='expenses'>
+            <ExpenseItem data={props.items[0]} />
+            <ExpenseItem data={props.items[1]} />
+        </div>
+    );
+};
+
+export default Expenses;
