02df21203a218ce4385d9d8b024a3344bb428212
alekseiildeikin <aleksei.ildeikin@voco.ee>
2026-09-21 08:15:14 +0000

    Change title

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,26 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.data.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+    };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.data.date}/>
                 <div className='expense-item__description'>
-                    <h2>{props.data.title}</h2>
+                    <h2>{title}</h2>
                     <div className='expense-item__price'>{props.data.price}</div>
                 </div>
-            </Card>
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
     )
 }
 
